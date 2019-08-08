724bec62746f1ec4636f6b8faaa8bc3932314175
6341 <6341@ait-sydney.net.au>
2019-08-08 17:02:07 +1000

    finished product detail

--- classes/Product.php
+++ classes/Product.php
@@ -25,26 +25,7 @@ class Product extends Database{
         }
         return $this -> products;
     }
-    //get product by id
-    public function getProductDetail( $id ){
-        $query = "SELECT
-        product_id,
-        name,
-        description,
-        price
-        FROM product
-        Where product_id = ? ";
 
-        $statement = $this -> connection -> prepare( $query );
-        $statement -> bind_param('i', $id );
-        if( $statement -> excecute()){
-            $product_detail = array();
-            $result = $statement -> get_result();
-            $row = $result -> fetch_assoc();
-            $product_detail['product'] = $row;
-            return $product_detail;        
-        }
-
-    }
+    
 }
 ?>
